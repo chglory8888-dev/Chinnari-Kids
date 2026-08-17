aff7c327b40b0b6deba59e29d5f4402ec189f06f
GloryGrace Challapalli <chglory8888@gmail.com>
2026-08-17 20:53:03 +0530

    Create sw.js

---
+++ public/sw.js
@@ -0,0 +1,50 @@
+const CACHE_NAME = "chinnaari-kids-v1";
+
+const APP_FILES = [
+  "/",
+  "/stories",
+  "/games",
+  "/learn",
+  "/puzzles",
+  "/colours",
+  "/dashboard"
+];
+
+self.addEventListener("install", (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.addAll(APP_FILES);
+    })
+  );
+
+  self.skipWaiting();
+});
+
+self.addEventListener("activate", (event) => {
+  event.waitUntil(
+    caches.keys().then((keys) =>
+      Promise.all(
+        keys
+          .filter((key) => key !== CACHE_NAME)
+          .map((key) => caches.delete(key))
+      )
+    )
+  );
+
+  self.clients.claim();
+});
+
+self.addEventListener("fetch", (event) => {
+  if (event.request.method !== "GET") return;
+
+  event.respondWith(
+    caches.match(event.request).then((cachedResponse) => {
+      return (
+        cachedResponse ||
+        fetch(event.request).catch(() => {
+          return caches.match("/");
+        })
+      );
+    })
+  );
+});
